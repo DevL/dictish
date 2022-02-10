6c34cfc2a2654d938c19c4054874283895fe8848
Lennart Fridén <lennart@devl.se>
2022-02-10 22:56:08 +0100

    Add support for get method

---
+++ tests/dictish_step_5_test.py
@@ -0,0 +1,19 @@
+from dictish_step_5 import Dictish
+
+DICTISH = Dictish([("a", 1), ("b", 2), ("c", 3)])
+
+
+def test_get_a_value_from_an_empty_dictish():
+    assert Dictish().get("missing") is None
+
+
+def test_get_a_value_from_a_dictish():
+    assert DICTISH.get("c") == 3
+
+
+def test_get_a_missing_value_from_a_dictish():
+    assert DICTISH.get("missing") is None
+
+
+def test_get_a_missing_value_from_a_dictish_using_a_default():
+    assert DICTISH.get("missing", "default") == "default"
