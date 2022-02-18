94111e47718b149e60c47fec7bd0e0e9b94676fe
Lennart Fridén <lennart@devl.se>
2022-02-18 23:12:46 +0100

    Add truth test

---
+++ tests/dictish_truth_test.py
@@ -0,0 +1,13 @@
+from dictish import Dictish
+
+
+AN_EMPTY_DICTISH = Dictish()
+LETTER_TO_NUMBER = Dictish([("a", 1), ("b", 2), ("c", 3)])
+
+
+def test_an_empty_dictish_is_falsey():
+    assert bool(AN_EMPTY_DICTISH) is False
+
+
+def test_a_populated_dictish_is_truthy():
+    assert bool(LETTER_TO_NUMBER) is True
